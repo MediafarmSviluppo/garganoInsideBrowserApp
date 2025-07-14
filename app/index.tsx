90ceff4ac82e7934bfe4662af575ce4a03a3db69
Riccardo Scaringella <rikirandom9@gmail.com>
2025-07-14 13:26:56 +0200

    Aggiunto reload su android

--- app/index.tsx
+++ app/index.tsx
@@ -1,62 +1,28 @@
+import CustomWebView from '@/components/customWebView';
 import React, {
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
+  useRef
 } from 'react';
-import { BackHandler, Linking, Platform, StatusBar, useColorScheme } from "react-native";
+import { StatusBar, useColorScheme } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { WebView } from 'react-native-webview';
 
-const defaultUrl = 'https://unterritoriodascoprire.it/'
 
 export default function Index() {
+  const defaultUrl = "https://unterritoriodascoprire.it/"
 
-  const colorScheme = useColorScheme();
-  const [canGoBack, setCanGoBack] = useState(false);
+  const colorScheme = useColorScheme();  
 
-  const webViewRef = useRef(null);
-
-  const onAndroidBackPress = useCallback(() => {
-    if (canGoBack) {
-      webViewRef.current?.goBack();
-      return true; // prevent default behavior (exit app)
-    }
-    return false;
-  }, [canGoBack]);
-
-useEffect(() => {
-  if (Platform.OS === 'android') {
-    const backHandler = BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
-    return () => {
-      backHandler.remove();
-    };
-  }
-}, [onAndroidBackPress]);
+  let webView = useRef(null);
 
   return (
     <>
     <StatusBar barStyle={`${colorScheme === "light" ? "dark": "light"}-content`} backgroundColor={colorScheme ==="light" ? "#fff":"#000"} translucent={true} />
     <SafeAreaView style={{ flex: 1, backgroundColor: "transparent" }}>
-      <WebView source={{ uri: defaultUrl }} 
-      onnavigationStateChange={onNavigationStateChange} 
-      pullToRefreshEnabled={true}
-      allowsBackForwardNavigationGestures={true} // per iOS
-      style={{ flex: 1 }} 
-      ref={webViewRef}
-      onLoadProgress={event => {
-        setCanGoBack(event.nativeEvent.canGoBack);
-      }}
-      />
+      <CustomWebView url={defaultUrl} webViewRef={webView} />
     </SafeAreaView>
     </>
   );
 }
 
-const onNavigationStateChange = (navState) => {
-  if (!navState.url.startsWith(defaultUrl)) {
-    Linking.openURL(navState.url)
-  }
-}
+
 
 
---
+++ components/customWebView.tsx
@@ -0,0 +1,78 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { BackHandler, Linking, Platform, RefreshControl, ScrollView, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+let defaultUrl = 'https://example.com'; // Replace with your default URL
+
+type CustomWebViewProps = {
+  url: string;
+  webViewRef: React.RefObject<null>;
+}
+
+const CustomWebView = (props: CustomWebViewProps) => {
+  defaultUrl = props.url || defaultUrl;
+
+  //#region Android back button handling
+    const [canGoBack, setCanGoBack] = useState(false);
+    const onAndroidBackPress = useCallback(() =>{
+      if (canGoBack) {
+          props.webViewRef.current?.goBack();
+          return true; // prevent default behavior (exit app)
+        }
+        return false;
+      }, 
+      [canGoBack]);
+
+    useEffect(() => {
+      if (Platform.OS === 'android') {
+        const backHandler = BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
+        return () => {
+          backHandler.remove();
+        };
+      }
+    }, [onAndroidBackPress]);
+
+    //#endregion
+
+    //#region Android pull to refresh
+    const [refreshing, setRefreshing] = React.useState(false);
+    const onRefresh = React.useCallback(() => {
+      setRefreshing(true);
+      props.webViewRef.current?.reload();
+      setTimeout(() => {
+        setRefreshing(false);
+      }, 2000);
+    }, []);
+    //#endregion
+
+    return (
+      <View style={{ flex: 1 }}>
+        <ScrollView 
+          contentContainerStyle={{ flex: 1 }}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }>
+            <WebView source={{ uri: props.url }} 
+                onNavigationStateChange={onNavigationStateChange} 
+                pullToRefreshEnabled={true}
+                allowsBackForwardNavigationGestures={true} // per iOS
+                style={{ flex: 1 }} 
+                ref={props.webViewRef}
+                onLoadProgress={event => {
+                    setCanGoBack(event.nativeEvent.canGoBack);
+                }}
+            />
+        </ScrollView>
+        </View>
+    )
+}
+
+//#region WebView functions
+const onNavigationStateChange = (navState) => {
+  if (!navState.url.startsWith(defaultUrl)) {
+    Linking.openURL(navState.url)
+  }
+}
+//#endregion
+
+export default CustomWebView;
